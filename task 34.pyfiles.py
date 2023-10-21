d329778358a64fd9a6bf5d4562e0669f5dc8d5e5
jamyangkairi <141301697+jamyangkairi@users.noreply.github.com>
2023-10-21 23:10:19 +0600

    Add files via upload

---
+++ task 34.pyfiles.py
@@ -0,0 +1,10 @@
+temperature = 99
+if temperature >= 100:
+    print("boiling")
+elif temperature > 98:
+    print("liquid")
+elif temperature > 10:
+    print("Freezing")
+else:
+    print("its cold")
+print("done")
